e9940e8a45dcbbae786cb9c65f10eb7a7d4aec9b
crisfroi <juanfroilanramos@gmail.com>
2025-07-07 09:24:02 +0100

    ProfessionalRegistration.tsx

--- src/pages/ProfessionalRegistration.tsx
+++ src/pages/ProfessionalRegistration.tsx
@@ -402,7 +402,7 @@ const ProfessionalRegistration = () => {
         </Form>
         
         <PoliticasModal open={showPoliticasModal} onClose={() => setShowPoliticasModal(false)} />
-        <PoliticasModal open={showPoliticasModal} onClose={() => setShowPoliticasModal(false)} />
+        <ProcedureModal isOpen={showProcedureModal} onClose={() => setShowProcedureModal(false)}/>
       </div>
     </div>
   );

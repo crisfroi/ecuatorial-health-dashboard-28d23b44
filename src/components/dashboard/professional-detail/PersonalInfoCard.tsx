4c5e0cd48471df5b04650b17833e47c591fff1b3
crisfroi <juanfroilanramos@gmail.com>
2025-07-14 02:15:00 +0100

    PersonalInfoCard.tsx

--- src/components/dashboard/professional-detail/PersonalInfoCard.tsx
+++ src/components/dashboard/professional-detail/PersonalInfoCard.tsx
@@ -35,7 +35,7 @@ const PersonalInfoCard = ({ professional }: PersonalInfoCardProps) => {
             <img
               src={professional.foto_carnet}
               alt="Foto del Carnet Profesional"
-              className="w-[162px] h-[212px] object-contain border-3 border-gray-300 rounded-lg mx-auto mb-2"
+              className="w-[162px] h-[212px] object-contain border-3 border-gray-300 rounded-lg mx-auto mb-2 shadow-lg"
               onError={(e) => {
                 // Fallback en caso de que la imagen no cargue
                 e.currentTarget.onerror = null; // Evita bucles infinitos
